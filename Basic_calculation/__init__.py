e0c4eb04de167112562ef7e886dcdc9ad02653d3
victor <chrollolucilfer@gmail.com>
2022-10-15 20:16:34 +0530

    made changes in the options entering from name to number , added methods for double integraion and triple integration

--- Basic_calculation/__init__.py
+++ Basic_calculation/__init__.py
@@ -1,13 +1,13 @@
 import re
 import Basic_calculation
 
+chapter_name = {'1 : calculus','2 : numerical Methods 1','3 : numerical Methods2','4 : Partial Differential Equations','5 : Vector calculus'}
+print(f"The chapters are:\n{chapter_name}")
+option = input("Enter the Chapter number: ")
 
-print("The chapters are:\ncalculus\tnumerical Methods")
-option = input("Enter the Chapter name: ")
-
-if (re.search('^cal', option)):
+if (option == '1'):
      from Basic_calculation import calculus
 
-if (re.search('^num', option)):
+if (option == '2'):
      from Basic_calculation import num_methods
 
--- Basic_calculation/calculus.py
+++ Basic_calculation/calculus.py
@@ -2,49 +2,90 @@ import sympy as smp
 from sympy import *
 import re
 
-
+# ♾️ == oo;  square_root = sqrt(val); exponential = exp(val)
 class Calculus:
     def __init__(self, expr, wrt_var):
         self.expr = expr
         self.wrt_var = wrt_var
 
     def indefinate(self):
-        print(integrate(self.expr, self.wrt_var))
+        ans = integrate(self.expr, self.wrt_var)
+        pprint(ans)
 
     def definate(self, l_limit, U_limit):
-        print(integrate(self.expr, (self.wrt_var, l_limit, U_limit)))
+        ans = integrate(self.expr, (self.wrt_var, l_limit, U_limit))
+        pprint(ans)
 
     def derivative(self):
-        print(diff(self.expr, self.wrt_var))
+        ans = diff(self.expr, self.wrt_var)
+        pprint(ans)
+
+    def double_integration(self, *args):
+        ans = integrate(self.expr, (self.wrt_var, args[0], args[1]), (args[2], args[3], args[4]))
+        pprint(ans)
     
-    def partial_derivative(self):
-        pass
+    def triple_integration(self, *args):
+        ans = integrate(self.expr, (self.wrt_var, args[0], args[1]), (args[2], args[3], args[4]), (args[5], args[6], args[7]))
+        pprint(ans)
+        
+
 
 
 def with_respect_to():
-    wrt_var = input("enter the with respect to variable:(x or y or z)")
+    wrt_var = input("enter the with respect to variable:(x or y or z)\n")
     return symbols(f"{wrt_var}")
 
 
-x, y, z = smp.symbols('x y z')
-print(f"Names of Operation:")
-print(f'definate Integration\tIndefinate Integration\tdifferentiation\tPartial Differentiation\n')
-expr = input("enter the expression in x and y terms:")
-opt = input("enter the operation:definate integration,indefinate integration,differentiation,etc:\n")
+def lower_upper_limit():
+    lower_limit = input('enter the lower limit:')
+    upper_limit = input ('enter the upper limit:')
+    return lower_limit, upper_limit
+    
 
-if (re.search('^indefinate', opt, re.IGNORECASE)):
+x, y, z, a, b = smp.symbols('x y z a b')
+init_printing(use_unicode = True)
+
+operation_name = {'1 : indefinate integration','2 : definate intergration','3 : differentiation','4 : double integration', '5 : triple integration'}
+option = input(f"enter the operation number:{operation_name}\n")
+expr = input("enter the expression in x and y terms:")
+pprint(simplify(expr))
+
+if (option == '1'):
     wrt_var = with_respect_to()
     obj1 = Calculus(expr, wrt_var)
     obj1.indefinate()
 
-if (re.search('^definate', opt, re.IGNORECASE)):
+if (option == '2'):
     wrt_var = with_respect_to()
-    lowerLimit = input("enter the lower limit of Integral:")
-    upperLimit = input("enter the upper limit of Integral:")
+    lower_limit, upper_limit = lower_upper_limit()
     obj2 = Calculus(expr, wrt_var)
-    obj2.definate(lowerLimit, upperLimit)
+    obj2.definate(lower_limit, upper_limit)
  
-if (re.search("^diff", opt, re.IGNORECASE)):
+if (option == '3'):
     wrt_var = with_respect_to()
     obj3 = Calculus(expr, wrt_var)
     obj3.derivative()
+
+if(option == '4'):
+    wrt_var = with_respect_to()
+    lower_limit, upper_limit = lower_upper_limit()
+  
+    wrt_var1 = with_respect_to()
+    lower_lim1, upper_lim1 = lower_upper_limit()
+   
+    obj4 = Calculus(expr, wrt_var)
+    obj4.double_integration(lower_limit, upper_limit, wrt_var1, lower_lim1, upper_lim1)
+
+if(option == '5'):
+    wrt_var = with_respect_to()
+    lower_limit, upper_limit = lower_upper_limit()
+
+    wrt_var1 = with_respect_to()
+    lower_lim1, upper_lim1 = lower_upper_limit()
+
+    wrt_var2 = with_respect_to()
+    lower_lim2, upper_lim2 = lower_upper_limit()
+
+    obj5 = Calculus(expr,wrt_var)
+    obj5.triple_integration(lower_limit, upper_limit, wrt_var1, lower_lim1, upper_lim1, wrt_var2, lower_lim2, upper_lim2)
+
