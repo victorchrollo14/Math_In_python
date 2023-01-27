fa2270acd626371166c94b2f47923a83f976bb22
Victor Immanuel <chrollolucilfer1402@gmail.com>
2023-01-27 11:38:33 +0530

    removed all unwanted code

--- Basic_calculation/__init__.py
+++ Basic_calculation/__init__.py
@@ -1,27 +1,13 @@
-
-
 # giving the users chapter names to select from
 
 
 chapter_name = {
-
-    0: "Sympy_features",
     1: "calculus",
-    2: "numerical methods",
-    4: "Partial differential Equations"
 }
 
 print(f"The chapters are:\n{chapter_name}")
 option = input(f"enter the chapter number: ")
 
-# if (int(option) == 0):
-#     from OtherChapters import sympy_feat
 
 if (int(option) == 1):
     from Basic_calculation import calculus
-
-# if (int(option) == 2):
-#     from OtherChapters import numerical_methods
-
-# if (int(option) == 4):
-#     from Basic_calculation import pde
